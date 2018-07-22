a25d76c88352eb2892e9b3c79e344b3f09232e1e
chendurex <chendurex@gmail.com>
2018-07-22 11:26:36 +0800

    修改日志级别

--- src/main/java/com/jianli/service/impl/UserServiceImpl.java
+++ src/main/java/com/jianli/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.jianli.response.ResResult;
 import com.jianli.response.ResUtils;
 import com.jianli.service.UserService;
 import com.jianli.wechat.AuthInvoker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
  * @date 2018-07-22 09:37
  */
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
     private final UserRepo userRepo;
     private final AuthInvoker authInvoker;
@@ -24,13 +26,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResResult submit(String code, String state) {
+        log.info("获取到用户code：{}， state：{}", code, state);
         UserParam param = authInvoker.getAccessToken(code, state);
+        log.info("获取到用户登录信息，param:{}", param);
         UserParam info = authInvoker.getUserInfo(param.getAccessToken(), param.getOpenid());
+        log.info("获取到用户信息，info:{}", info);
         User user = User.builder().nickname(info.getNickname()).headImgUrl(info.getHeadImgUrl()).country(info.getCountry())
                 .province(info.getProvince()).city(info.getCity()).sex(info.getSex()).unionId(info.getUnionId())
                 .accessToken(param.getAccessToken()).expiresIn(param.getExpiresIn())
                 .openid(param.getOpenid()).refreshToken(param.getRefreshToken()).scope(param.getScope())
                 .build();
+
         User origin = userRepo.get(user.getOpenid());
         User real;
         if (origin == null) {
