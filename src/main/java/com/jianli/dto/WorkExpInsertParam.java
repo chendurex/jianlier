7b8e14f612d07346a9c012549ea14bb456dbd8fa
cheny.huang <cheny.huang@corp.to8to.com>
2018-08-14 08:49:58 +0800

    新增工作工作必须填写简历ID

--- src/main/java/com/jianli/dto/WorkExpInsertParam.java
+++ src/main/java/com/jianli/dto/WorkExpInsertParam.java
@@ -2,10 +2,11 @@ package com.jianli.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author chendurex
@@ -18,7 +19,7 @@ public class WorkExpInsertParam extends WorkExpParam {
 
     @ApiModelProperty(notes = "简历ID", example = "1", required = true)
     @Min(value= 1, message = "简历ID必须大于1")
-    @NotBlank
+    @NotNull
     private Integer resumeId;
 
 }
