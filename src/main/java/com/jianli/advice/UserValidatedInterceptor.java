b42c282436ed421f938729f8ce93ca62613ab211
chendurex <chendurex@gmail.com>
2018-09-20 23:37:08 +0800

    优化身份验证

--- src/main/java/com/jianli/advice/UserValidatedInterceptor.java
+++ src/main/java/com/jianli/advice/UserValidatedInterceptor.java
@@ -10,9 +10,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
 /**
  * 验证当前操作是否属于当前登录用户，如果不是则直接提示错误
@@ -39,7 +36,7 @@ public class UserValidatedInterceptor extends HandlerInterceptorAdapter {
         if (StringUtils.isEmpty(uid) || StringUtils.isEmpty(ticket)) {
             throw new AuthenticException();
         }
-        boolean suc = userService.isOwner(StringUtils.toInt(request.getHeader(UID)), request.getHeader(TICKET));
+        boolean suc = userService.isOwner(StringUtils.toInt(uid), ticket);
         if (suc) {
             return true;
         }
